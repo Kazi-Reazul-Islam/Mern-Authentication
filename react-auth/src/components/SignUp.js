632822119d12f9194f9863ef623ad2553069ec65
Kazi Reazul Islam <kazireazulislam1999@gmail.com>
2023-02-18 12:45:41 +0600

    signup css added

--- react-auth/src/components/SignUp.js
+++ react-auth/src/components/SignUp.js
@@ -1,7 +1,25 @@
 import React from "react";
 
 function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <>
+      <h1 className="center">SIGN UP</h1>
+      <div className="outcard">
+        <label>Email: </label>
+        <br />
+        <input className="inputs" type="email" />
+        <br />
+        <br />
+
+        <label>Password: </label>
+        <input className="inputs" type="password" />
+        <br />
+        <br />
+
+        <button className="btns">SUBMIT</button>
+      </div>
+    </>
+  );
 }
 
 export default SignUp;
