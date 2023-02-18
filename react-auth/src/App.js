9963902395a6595c92e9d0559e2a41acc0d2e12f
Kazi Reazul Islam <kazireazulislam1999@gmail.com>
2023-02-18 11:47:22 +0600

    routes added

--- react-auth/src/App.js
+++ react-auth/src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import SignUp from "./components/SignUp";
+import SignIn from "./components/SignIn";
+import Home from "./components/Home";
 
-const App = () => {
-  return <div>Hello</div>;
-};
+function App() {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signin" element={<SignIn />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
 
 export default App;
